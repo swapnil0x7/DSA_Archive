6566b08e833826a39d33c4a200036766d8d37c90
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-06-12 23:09:33 +0530

    Find Kth largest and smallest element in given array

--- Heaps/KthLargestSmallest.cpp
+++ Heaps/KthLargestSmallest.cpp
@@ -40,3 +40,44 @@ vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
 //for Kth Largest we should use minHeap for better TC
 // max Heap, TC = O(N + klog(N))
 // min Heap, TC = O(k + (n-k)log(K)) which is slightly better! 
+
+
+
+//ALternate solution as discussed above.
+void kth_Largest_MaxHeap(vector<int>&arr, int k) {
+
+	priority_queue<int>pq ;
+	int n = arr.size()  ;
+
+	for (int i = 0; i < arr.size(); i++) {
+	    pq.push(arr[i])  ;
+	}
+
+	int f = k - 1 ;
+
+	while (f > 0) {
+	    pq.pop()  ;
+	    f-- ;
+	}
+
+	cout << "Kth Largest element " << pq.top() << "\n"  ;
+}
+
+void kth_Smallest_MinHeap(vector<int>&arr, int k) {
+
+	priority_queue<int, vector<int>, greater<int>>pq ;
+	int n = arr.size()  ;
+
+	for (int i = 0; i < arr.size(); i++) {
+	    pq.push(arr[i])  ;
+	}
+
+	int f = k - 1 ;
+
+	while (f > 0) {
+	    pq.pop()  ;
+	    f-- ;
+	}
+
+	cout << "Kth Smallest element " << pq.top() << "\n"  ;
+}
