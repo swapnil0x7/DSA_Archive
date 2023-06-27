bdf1193481591b6fed8d618c80fec3dc53f640b0
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-06-27 16:27:25 +0530

    Add files via upload

---
+++ Binary Search/RoseGarden.cpp
@@ -0,0 +1,41 @@
+Link - https://www.codingninjas.com/studio/problems/rose-garden_2248080?utm_source=youtube&utm_medium=affiliate&utm_campaign=codestudio_Striver_BinarySeries&leftPanelTab=0
+
+bool isPossible(vector<int> &arr, int mid, int m, int k){
+	int count = 0, noOfBouquets = 0;
+
+	for(int i=0;i<arr.size();i++){
+		if(arr[i] <= mid){
+			count++;
+		}
+		else{
+			noOfBouquets += count/k;
+			count = 0;
+		}
+	}
+	noOfBouquets += count/k;
+	return noOfBouquets >= m;
+}
+
+int roseGarden(vector<int> arr, int k, int m)
+{	
+	int n = arr.size();
+	long long int totalRequired = k*1LL * m*1LL;
+	if(n < totalRequired) return -1;
+
+	int minimum = 0, maximum = 0;
+	for(int i=0;i<n;i++){
+		minimum = min(minimum, arr[i]);
+		maximum = max(maximum, arr[i]);
+	}
+	int low = minimum, high = maximum;
+
+	while(low <= high){
+		int mid = (low+high)/2;
+
+		if(isPossible(arr, mid, m, k))
+			high = mid - 1;
+		else
+			low = mid + 1;
+	}
+	return low;
+}
