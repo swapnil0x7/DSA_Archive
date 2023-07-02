406298f2e871d635773e2f038bae73f38d89c4e4
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-07-03 00:38:43 +0530

    LCA

--- Binary Tree/LCA.cpp
+++ Binary Tree/LCA.cpp
@@ -21,3 +21,42 @@ int lowestCommonAncestor(TreeNode<int> *root, int x, int y)
     TreeNode<int>* ans = lca(root, x, y);
     return ans->data;
 }
+
+bool path(TreeNode* root, int target, vector<int> &p){
+    if(!root) return false;
+    
+    if(root->val == target){
+        p.push_back(root->val);
+        return true;
+    }
+
+    if(path(root->left, target, p) or path(root->right, target, p)){
+        p.push_back(root->val);
+        return true;
+    }
+    return false;
+}
+
+//Approach two - takes extra space though
+int Solution::lca(TreeNode* root, int x, int y) {
+    vector<int> pathX;
+    vector<int> pathY;
+
+    path(root, x, pathX);
+    path(root, y, pathY);
+
+    int n = pathX.size() - 1;
+    int m = pathY.size() -1;
+    int ans = -1;
+
+    while(n>=0 and m >=0){
+        if(pathX[n] == pathY[m]){
+            ans = pathX[n];
+            n--;
+            m--;
+        }
+        else break;
+    }
+    return ans;
+}
+
