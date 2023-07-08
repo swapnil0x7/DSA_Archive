936aaa6f1d950ba63094c452570a2177f43cdd19
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-07-09 00:11:26 +0530

    DFS technique

--- Graph/topological-sort.cpp
+++ Graph/topological-sort.cpp
@@ -1,28 +1,29 @@
-	void dfs(int node, vector<int> adj[], vector<int> &visited, stack<int> &s){
-	    visited[node] = 1;
-	    
-	    for(auto j: adj[node]){
-	        if(!visited[j]){
-	            dfs(j, adj, visited, s);
-	        }
-	    }
-	    s.push(node);
+
+void dfs(int node, vector<int> adj[], vector<int> &visited, stack<int> &s){
+    visited[node] = 1;
+    
+    for(auto j: adj[node]){
+	if(!visited[j]){
+	    dfs(j, adj, visited, s);
 	}
-	 
-	vector<int> topoSort(int n, vector<int> adj[]) 
-	{
-	    vector<int> res;
-	    stack<int> s;
-	    vector<int> visited(n,0);
-	    
-	    for(int i=0;i<n;i++){
-	        if(!visited[i])
-	            dfs(i, adj, visited, s);
-	    }
-	    
-	    while(!s.empty()){
-	        res.push_back(s.top());
-	        s.pop();
-	    }
-	    return res;
-	}
+    }
+    s.push(node);
+}
+ 
+vector<int> topoSort(int n, vector<int> adj[]) 
+{
+    vector<int> res;
+    stack<int> s;
+    vector<int> visited(n,0);
+    
+    for(int i=0;i<n;i++){
+	if(!visited[i])
+	    dfs(i, adj, visited, s);
+    }
+    
+    while(!s.empty()){
+	res.push_back(s.top());
+	s.pop();
+    }
+    return res;
+}
