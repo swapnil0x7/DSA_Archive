f298864c18a2546a4d44f16e66e1427b5aa2b08f
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-07-09 00:38:04 +0530

    Both BFS and DFS

--- Graph/Detect-cycle-directed.cpp
+++ Graph/Detect-cycle-directed.cpp
@@ -31,3 +31,37 @@ bool isCyclic(int n, vector<int> adj[]) {
     }
     return false;
 }
+
+// BFS - using Kahn's algorithm
+// since Topological sort works only on DIRECTED-ACYCLIC-GRAPHS only therefore if kahn's algo fails to produce the topo-sort
+// of the given directed graph ( topo_sort size != N ), It is CYCLIC!!!
+bool isCyclic(int n, vector<int> adj[]) {
+        
+    vector<int> inDegree(n, 0);                                    // line 40 to 63 is just topo-sort algo!
+    
+    for(int i=0;i<n;i++){
+        for(auto j: adj[i]) inDegree[j]++;
+    }
+    
+    queue<int> q;
+    
+    for(int i=0;i<n;i++){
+        if(inDegree[i] == 0) q.push(i);
+    }
+    
+    vector<int> res;
+    
+    while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        res.push_back(curr);
+        
+        for(auto j: adj[curr]){
+            inDegree[j]--;
+            if(inDegree[j] == 0) q.push(j);
+        }
+    }
+    if(res.size() != n)    
+        return true;
+       return false;
+}
