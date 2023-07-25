4578e98151956732fb86b3b08516e30215065996
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-07-26 03:26:06 +0530

    search-In-Rotated-Sorted-Array-II

---
+++ Binary Search/search-In-Rotated-Sorted-Array-II.cpp
@@ -0,0 +1,31 @@
+bool searchInARotatedSortedArrayII(vector<int>&arr, int x) {
+    int n = arr.size();
+    int low = 0, high = n-1;
+    
+    while(low <= high){
+        int mid = low + (high - low)/2;
+
+        if(arr[mid] == x) return true;
+
+        if(arr[low] == arr[mid] and arr[mid] == arr[high]){
+            low++;
+            high--;
+            continue;   
+        }
+
+        if(arr[low] <= arr[mid]){
+            if(x >= arr[low] and x < arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else{
+            if(x > arr[mid] and x <= arr[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+    return false;
+
+}
