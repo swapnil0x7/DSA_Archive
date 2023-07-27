1f80ff8e93e4688adeef3f6922fac708cabc2023
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-07-27 14:04:46 +0530

    Minimum element in sorted array.cpp to Minimum in Rotated Sorted Array I

--- Binary Search/Minimum in Rotated Sorted Array I.cpp
+++ Binary Search/Minimum in Rotated Sorted Array I.cpp
@@ -1,16 +1,15 @@
 // we first look for the sorted part because the first element of sorted part might be our possible ans.
 // so we keep the first element and remove the sorted part from out search space!
+ 
 
-
-int findMin(vector<int>& arr)
-{
+int findMin(vector<int>& arr) {
 	int n = arr.size();
 	int low = 0, high = n-1;
 	int ans = 1e9;
 	
 	while(low <= high){
 		int mid = low + (high-low)/2;
-
+	
 		if(arr[low] <= arr[mid]){          
 			ans = min(ans,arr[low]);
 			low = mid + 1;
