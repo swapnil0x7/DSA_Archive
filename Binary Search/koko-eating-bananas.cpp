6129f4651cd62b187a2aad7dfca72ee7eb9b7211
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-07-28 21:56:03 +0530

    koko-eating-bananas

--- Binary Search/koko-eating-bananas.cpp
+++ Binary Search/koko-eating-bananas.cpp
@@ -1,28 +1,28 @@
 // Time Complexity - O(N * log(maxElement))
 
 int calculateTime(vector<int> &arr, int rate){
-    int n = arr.size();
     int time = 0;
-    for(int i=0;i<n;i++){
-        time += ceil((double)arr[i] / (double)rate);
+    for(int i=0;i<arr.size();i++){
+        time += ceil((double)arr[i] / rate);
     }
     return time;
 }
 
 int minimumRateToEatBananas(vector<int> arr, int h) {
+    
     int n = arr.size();
     int maximum = 1e9;
-    for(int i=0;i<n;i++){
-        maximum = max(maximum, arr[i]);
-    }
-    
-    int low = 1, high = maximum;
     int ans = maximum;
+    for(int i : arr) {
+        maximum = max(maximum, i);
+    }
+    int low = 1, high = maximum;
     
     while(low <= high){
         int mid = low + (high-low)/2;
+
         int timeTaken = calculateTime(arr, mid);
-    
+
         if(timeTaken <= h){
             ans = mid;
             high = mid - 1;
@@ -32,4 +32,5 @@ int minimumRateToEatBananas(vector<int> arr, int h) {
         }
     }
     return ans;
+    
 }
