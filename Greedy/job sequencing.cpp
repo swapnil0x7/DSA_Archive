772cf8158c24f3785e9697604ba9d7e719b2f323
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-08-05 05:43:24 +0530

    O(N log N) + O(N*M)

--- Greedy/job sequencing.cpp
+++ Greedy/job sequencing.cpp
@@ -1,31 +1,31 @@
 
-  bool static compare(Job a, Job b){
-      return a.profit > b.profit;
-  }
-  
-  vector<int> JobScheduling(Job arr[], int n) 
-  { 
-       sort(arr, arr+n, compare);
-       
-       int maxi = arr[0].dead;
-       
-       for(int i=0;i<n;i++){
-           maxi = max(maxi, arr[i].dead);
-       }
-       
-       vector<bool> slots(maxi + 1, false);
-       
-       int jobs = 0, profit = 0;
-       
-       for(int i=0;i<n;i++){
-           for(int j=arr[i].dead; j>0; j--){
-               if(slots[j] == false){
-                   slots[j] = true;
-                   jobs++;
-                   profit += arr[i].profit;
-                   break;
-               }
-           }
-       }
-       return {jobs, profit};
-  } 
+bool static compare(Job a, Job b){
+    return a.profit > b.profit;
+}
+
+vector<int> JobScheduling(Job arr[], int n) 
+{ 
+     sort(arr, arr+n, compare);
+     
+     int maxi = arr[0].dead;
+     
+     for(int i=0;i<n;i++){
+         maxi = max(maxi, arr[i].dead);
+     }
+     
+     vector<bool> slots(maxi + 1, false);
+     
+     int jobs = 0, profit = 0;
+     
+     for(int i=0;i<n;i++){
+         for(int j=arr[i].dead; j>0; j--){
+             if(slots[j] == false){
+                 slots[j] = true;
+                 jobs++;
+                 profit += arr[i].profit;
+                 break;
+             }
+         }
+     }
+     return {jobs, profit};
+} 
