bcf35343acc63b44019e0a85cdcde362eda34eff
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-09-08 20:18:37 +0530

    Update PartitionEqualSubsetSum.cpp

--- Dynamic programming/PartitionEqualSubsetSum.cpp
+++ Dynamic programming/PartitionEqualSubsetSum.cpp
@@ -29,3 +29,58 @@ bool canPartition(vector<int> &arr, int n)
 	
 	return f(n, target, arr);
 }
+
+// Tabulation
+bool canPartition(vector<int>& arr) {
+        int n = arr.size();
+        int sum = 0;
+        for(int e: arr) sum += e;
+
+        if(sum % 2 != 0) return false;
+        sum = sum/2;
+        
+        vector<vector<int>> dp(n, vector<int>(sum+1, 0));
+	
+	    //base cases
+        for(int i=0;i<n;i++) dp[i][0] = true;
+        if(arr[0] <= sum) dp[0][arr[0]] = true;			// make sure to put this condition!
+
+        for(int i=1;i<n;i++){
+            for(int target=1;target<=sum;target++){
+                bool take = false;
+                if(arr[i] <= target) take = dp[i-1][target-arr[i]];
+                bool not_take = dp[i-1][target];
+                
+                dp[i][target] = take or not_take;
+            }
+        }
+        return dp[n-1][sum];	
+}
+
+// Space optimised
+bool canPartition(vector<int>& arr) {
+	int n = arr.size();
+	int sum = 0;
+	for(int e: arr) sum += e;
+	
+	if(sum % 2 != 0) return false;
+	sum = sum/2;
+	
+	vector<int>prev(sum+1, 0), curr(sum+1, 0);
+	    
+	//base cases
+	for(int i=0;i<n;i++) prev[0] = true;
+	if(arr[0] <= sum) prev[arr[0]] = true;
+	
+	for(int i=1;i<n;i++){
+	    for(int target=1;target<=sum;target++){
+		bool take = false;
+		if(arr[i] <= target) take = prev[target-arr[i]];
+		bool not_take = prev[target];
+		
+		curr[target] = take or not_take;
+	    }
+	    prev = curr;
+	}
+	return prev[sum];	
+}
