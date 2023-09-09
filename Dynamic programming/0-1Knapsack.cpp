11a1ea7ca94ece8184b1760938b1bb9963d3d8d9
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-09-09 15:43:13 +0530

    Update 0-1Knapsack.cpp

--- Dynamic programming/0-1Knapsack.cpp
+++ Dynamic programming/0-1Knapsack.cpp
@@ -44,3 +44,22 @@ int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
 	return dp[n-1][maxWeight];
 }
 
+// space optimization
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{	
+	vector<int> prev(maxWeight+1, 0), curr(maxWeight+1, 0);
+	for(int w=0;w<=maxWeight;w++){
+		if(w>=weight[0]) prev[w] = value[0];
+	}
+	for(int i=1;i<n;i++){
+		for(int W=0;W<=maxWeight;W++){
+			int not_take = prev[W];
+			int take = -1e9;
+			if(weight[i] <= W) take = value[i] + prev[W-weight[i]];
+	
+			curr[W] = max(take, not_take);
+		}
+		prev = curr;
+	}
+	return prev[maxWeight];
+}
