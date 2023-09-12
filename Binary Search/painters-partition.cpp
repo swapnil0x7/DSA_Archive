cd632b1f546e86a61360d808e44635d9a099c40b
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2023-09-13 01:32:06 +0530

    Update painters-partition.cpp

--- Binary Search/painters-partition.cpp
+++ Binary Search/painters-partition.cpp
@@ -1,3 +1,4 @@
+// variation 1
 int countPainters(vector<int> &arr, int time_limit){
     int painters = 1, time = 0;
     for(int i=0;i<arr.size();i++){
@@ -37,3 +38,45 @@ int findLargestMinDistance(vector<int> &arr, int k)
     }
     return ans;
 }
+
+// variation 2 (asked in mock interview) - https://www.interviewbit.com/problems/painters-partition-problem/
+int mod = 10000003;
+int countPainters(vector<int> &arr, int time_limit){
+    int painters = 1, time = 0;
+    for(int i=0;i<arr.size();i++){
+        if(time + arr[i] <= time_limit){
+            time = time +  arr[i];
+        }
+        else{
+            painters++;
+            time = arr[i];
+        }
+    }
+    return painters;
+}
+
+int Solution::paint(int A, int B, vector<int> &arr) {
+    int n = arr.size();
+    
+    int max_element = 0, total = 0;
+    for(int i=0;i<n;i++){
+        max_element = max(max_element, arr[i]);
+        total  = total + arr[i];
+    }
+    
+    int low = (max_element * B), high = (total * B);
+    int ans = 0;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        
+        if(countPainters(arr, mid) <= A){
+            ans = mid;
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
