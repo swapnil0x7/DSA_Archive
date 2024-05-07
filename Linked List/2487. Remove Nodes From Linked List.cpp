a147c7d10665a575c7d641ba48b27922d5a79b33
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2024-05-08 04:32:43 +0530

    Update 2487. Remove Nodes From Linked List.cpp

--- Linked List/2487. Remove Nodes From Linked List.cpp
+++ Linked List/2487. Remove Nodes From Linked List.cpp
@@ -1,13 +1,5 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+// Space - O(N)
 class Solution {
 public:
     ListNode* reverseList(ListNode* head){
@@ -40,6 +32,42 @@ public:
             res->next = s.top();
             s.pop();
             res = res->next;
+        }
+        res->next = NULL;
+        result = reverseList(result->next);
+        return result;
+    }
+};
+
+// Space - O(1)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head){
+        ListNode* curr = head, *prev = NULL;
+        while(curr){
+            ListNode* NEXT = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = NEXT;
+        }
+        return prev;
+    }
+
+    ListNode* removeNodes(ListNode* head) {
+        ListNode* curr = head;
+        curr = reverseList(curr);
+        int largestOnLeft = -1e9+7;
+
+        ListNode* result = new ListNode(-1);
+        ListNode* res = result;
+
+        while(curr){
+            if(curr->val >= largestOnLeft){
+                largestOnLeft = curr->val;
+                res->next = curr;
+                res = res->next;
+            }
+            curr = curr->next;
         }
         res->next = NULL;
         result = reverseList(result->next);
