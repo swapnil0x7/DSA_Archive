db7405e83e415457ef000b6e988150b873d9b5a8
Swapnil Kumar <66438846+swapnil0x7@users.noreply.github.com>
2024-05-21 02:08:16 +0530

    Update allPathsFromRootToLeafNode.cpp

--- Binary Tree/allPathsFromRootToLeafNode.cpp
+++ Binary Tree/allPathsFromRootToLeafNode.cpp
@@ -14,6 +14,27 @@ void rootToLeafPaths(TreeNode<int>* root, vector<vector<int>> &res, vector<int> 
     
 }
 
+// OR
+  
+void f(Node* root, vector<int> &ans, vector<vector<int>> &res){
+    if(!root) return;
+    ans.push_back(root->data);
+    
+    if(!root->left and !root->right) res.push_back(ans);
+    
+    f(root->left, ans, res);
+    f(root->right, ans, res);
+    
+    ans.pop_back();
+}
+
+vector<vector<int>> Paths(Node* root) {
+    vector<vector<int>> res;
+    vector<int> ans;
+    f(root, ans, res);
+    return res;
+}
+
 
 
 vector<int> allPathsFromRootToLeaf(TreeNode<int> *root)
